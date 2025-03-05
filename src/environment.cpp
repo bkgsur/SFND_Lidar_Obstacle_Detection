2f308060a5aede0225a4ec3d90950fc72ed075a2
bkgsur <bkgsur@yahoo.com>
2025-03-04 21:24:33 -0800

    Initial commit

--- src/environment.cpp
+++ src/environment.cpp
@@ -7,7 +7,7 @@
 #include "processPointClouds.h"
 // using templates for processPointClouds so also include .cpp to help linker
 #include "processPointClouds.cpp"
-
+#include <boost/filesystem.hpp>
 std::vector<Car> initHighway(bool renderScene, pcl::visualization::PCLVisualizer::Ptr& viewer)
 {
 
