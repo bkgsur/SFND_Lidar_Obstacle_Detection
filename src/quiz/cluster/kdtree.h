9b91c64c9a88a4e3a9760d99e8a13a0383222f9e
bkgsur <bkgsur@yahoo.com>
2025-03-07 22:03:22 -0800

    Clustering

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -12,8 +12,7 @@ struct Node
 	Node* left;
 	Node* right;
 
-	Node(std::vector<float> arr, int setId)
-	:	point(arr), id(setId), left(NULL), right(NULL)
+	Node(std::vector<float> arr, int setId):point(arr), id(setId), left(NULL), right(NULL)
 	{}
 
 	~Node()
@@ -27,19 +26,37 @@ struct KdTree
 {
 	Node* root;
 
-	KdTree()
-	: root(NULL)
-	{}
+	KdTree():root(NULL)	{}
 
 	~KdTree()
 	{
 		delete root;
 	}
 
+	void insertHelper(Node** node, uint depth, std::vector<float> point, int id)
+	{
+		if(*node ==NULL)
+		{
+			*node = new Node(point,id);
+		}
+		else
+		{
+			 uint cd = depth%2;
+			 if(point[cd] <((*node)->point[cd]))
+			 {
+				insertHelper(&((*node)->left),   depth+1,  point,  id);
+			 }
+			 else
+			 {
+				insertHelper(&((*node)->right),   depth+1,  point,  id);
+			 }
+
+		}
+	}
+
 	void insert(std::vector<float> point, int id)
 	{
-		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+		insertHelper(&root, 0,  point, id);
 
 	}
 
